791785728c2e8c503c8e0ed823e7fe8fc94d8c82
Luis Felipe Graciolli <shiwa101@proton.me>
2026-01-31 16:08:31 -0300

    refactor: Makes A single paddle for player 1 and 2

--- Scripts/player_1_paddle.gd
+++ Scripts/player_1_paddle.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 
+@export var player_input_type: int
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -8,7 +10,13 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("p1_up", "p1_down")
+	var direction: float
+	
+	if player_input_type == 1:
+		direction = Input.get_axis("p1_up", "p1_down")
+	if player_input_type == 2:
+		direction = Input.get_axis("p2_up", "p2_down")
+		
 	if direction:
 		velocity.y = direction * SPEED
 	else:
