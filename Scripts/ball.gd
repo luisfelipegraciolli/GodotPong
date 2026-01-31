23474685bacc86a87521c05cb1f923afd7299ed8
Luis Felipe Graciolli <shiwa101@proton.me>
2026-01-31 17:28:42 -0300

    feat: Adds ball and colisions

---
+++ Scripts/ball.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+@onready var tile_map_layer_arena: TileMapLayer = $"../Map Design/TileMapLayer Arena"
+
+const INITIAL_SPEED = 100
+var speed = INITIAL_SPEED
+var direction: Vector2 = Vector2.LEFT
+@onready var initial_pos = position
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	speed += delta * 2
+	position += speed * direction * delta
+	
+func reset():
+	direction = Vector2.LEFT
+	position = initial_pos
+	speed = INITIAL_SPEED
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Paddle:
+		if body.player_input_type == 1:
+			direction = Vector2(1, randf() * 2 - 1).normalized()
+		else:
+			direction = Vector2(-1, randf() * 2 - 1).normalized()
+	else:
+		print(position.y)
+		# Celling
+		if position.y < -70:
+			direction = (direction + Vector2(0, 1)).normalized()
+		# Floor
+		elif position.y > 70:
+			direction = (direction + Vector2(0, -1)).normalized()
+			
