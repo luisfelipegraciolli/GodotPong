75750bc713bd28104a16b8b4074edea5fbe8c99b
Luis Felipe Graciolli <shiwa101@proton.me>
2026-01-31 17:50:48 -0300

    feat: Resets ball once out of bounds

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,14 +1,15 @@
+class_name Ball
 extends Area2D
 
 @onready var tile_map_layer_arena: TileMapLayer = $"../Map Design/TileMapLayer Arena"
 
-const INITIAL_SPEED = 100
+const INITIAL_SPEED = 200
 var speed = INITIAL_SPEED
 var direction: Vector2 = Vector2.LEFT
 @onready var initial_pos = position
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	speed += delta * 2
+	speed += delta * 3
 	position += speed * direction * delta
 	
 func reset():
---
+++ Scripts/wall.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+	
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Ball:
+		area.reset()
+		
