d8915e8d43560b1d97d22efa467315c31ae17343
Luis Felipe Graciolli <shiwa101@proton.me>
2026-02-01 11:51:22 -0300

    refactor: Change Scene filepath to a ScenePacked exported variable

--- Scenes/UI/main_menu.gd
+++ Scenes/UI/main_menu.gd
@@ -1,25 +1,12 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+@export var main_game_scene: PackedScene
+@export var controls_scene: PackedScene
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")
-	
-
-
+	get_tree().change_scene_to_packed(main_game_scene)
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_controls_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/UI/controls.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene_to_packed(controls_scene)
