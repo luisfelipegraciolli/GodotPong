d99253bb53c1834a1c44e196cb9def2b5ba20f0a
Luis Felipe Graciolli <shiwa101@proton.me>
2026-01-31 21:10:35 -0300

    feat: Removes commands text on main game UI

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -4,4 +4,5 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
 	
-	
+	if Input.is_action_just_pressed("menu"):
+		get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
